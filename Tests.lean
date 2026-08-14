b131e836349f15794d9181319b9834212f705c5e
Dake Bu <159223240+DakeBU@users.noreply.github.com>
2026-08-14 18:42:55 +0800

    Run dyadic energy stopping tests

--- Tests.lean
+++ Tests.lean
@@ -9,6 +9,7 @@ import Tests.DiscreteDoobL2
 import Tests.ContinuousDoobL2
 import Tests.DyadicElementaryRefinement
 import Tests.DyadicElementaryStopping
+import Tests.DyadicEnergyStopping
 import Tests.ElementaryItoIntegral
 import Tests.ElementaryItoAlgebra
 import Tests.ElementaryItoEmbedding
