f3b4c57e2294976b64751da6a44a733d331ad28c
Dake Bu <159223240+DakeBU@users.noreply.github.com>
2026-08-16 02:42:00 +0800

    Run generic progressive L2 stopping test

--- Tests.lean
+++ Tests.lean
@@ -37,6 +37,7 @@ import Tests.ProgressiveL2
 import Tests.ProgressiveL2Density
 import Tests.ProgressiveL2Algebra
 import Tests.ProgressiveL2Truncation
+import Tests.ProgressiveL2Stopping
 import Tests.RandomStoppingDyadicApprox
 import Tests.RandomStoppingBoundary
 import Tests.RandomStoppingProcessApprox
