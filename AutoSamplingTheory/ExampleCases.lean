7f3d94385f1ee0b645933e860762c91ebc1ccaf8
Ji Cheng <jicheng9617@gmail.com>
2026-09-11 15:30:13 +0800

    Admit finite recursive KL and begin source Gaussian budget

--- AutoSamplingTheory/ExampleCases.lean
+++ AutoSamplingTheory/ExampleCases.lean
@@ -27,6 +27,7 @@ import AutoSamplingTheory.ExampleCases.SmoothedPicardHMC.AdaptiveKLError
 import AutoSamplingTheory.ExampleCases.SmoothedPicardHMC.StateDependentRGO
 import AutoSamplingTheory.ExampleCases.SmoothedPicardHMC.FiniteRGOProgram
 import AutoSamplingTheory.ExampleCases.SmoothedPicardHMC.StoppedRGODepth
+import AutoSamplingTheory.ExampleCases.SmoothedPicardHMC.FiniteRGOKLError
 
 /-!
 # ASTIS example cases
