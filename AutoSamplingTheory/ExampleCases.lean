0d89845910b9a1ec0bf9c1e68d1992c7ba72d1fe
Ji Cheng <jicheng9617@gmail.com>
2026-09-12 09:36:25 +0800

    Integrate actual terminal FORS module into canonical Lean roots

--- AutoSamplingTheory/ExampleCases.lean
+++ AutoSamplingTheory/ExampleCases.lean
@@ -46,6 +46,7 @@ import AutoSamplingTheory.ExampleCases.SmoothedPicardHMC.JointReferenceGradientD
 import AutoSamplingTheory.ExampleCases.SmoothedPicardHMC.NormalizedReferenceCall
 import AutoSamplingTheory.ExampleCases.SmoothedPicardHMC.ReferenceCarryingKernel
 import AutoSamplingTheory.ExampleCases.SmoothedPicardHMC.ReferenceCarryingCost
+import AutoSamplingTheory.ExampleCases.SmoothedPicardHMC.TerminalFORSKernel
 import AutoSamplingTheory.ExampleCases.SmoothedPicardHMC.ClippedGradientProgram
 
 /-!
